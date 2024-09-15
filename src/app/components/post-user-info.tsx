cba3268319ee024f80d1e4bbb77d330937aa74f2
jefersonapps <jefersonplaynunes@gmail.com>
2024-09-15 12:59:26 -0300

    feat: conditionally apply capitalize styling for author names if not an email

--- src/app/components/post-user-info.tsx
+++ src/app/components/post-user-info.tsx
@@ -3,12 +3,17 @@
 import Image from "next/image";
 import { Post, UnauthenticatedPosts } from "../db/actions";
 import { formatDateString } from "../utils/helpers";
+import z from "zod";
+
+const emailSchema = z.string().email();
 
 interface PostUserInfoProps {
   post: Post | UnauthenticatedPosts;
 }
 
 export function PostUserInfo({ post }: PostUserInfoProps) {
+  const isEmail = emailSchema.safeParse(post?.author).success;
+
   return (
     <div className="flex items-center gap-4">
       <Image
@@ -19,7 +24,10 @@ export function PostUserInfo({ post }: PostUserInfoProps) {
         className="rounded-full"
       />
       <div className="flex flex-col">
-        <span className="text-md font-semibold text-zinc-900 dark:text-zinc-300 capitalize">
+        <span
+          data-capitalize={!isEmail}
+          className="text-md font-semibold text-zinc-900 dark:text-zinc-300 data-[capitalize=true]:capitalize"
+        >
           {post?.author}
         </span>
         <span className="text-xs text-gray-500">
