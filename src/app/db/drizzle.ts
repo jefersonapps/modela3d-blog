d6bc7b1c573a7854f9770c478bfa58769a3194b1
jefersonapps <jefersonplaynunes@gmail.com>
2024-10-16 14:04:27 -0300

    refactor: explicitly add users table to Drizzle schema

--- src/app/db/drizzle.ts
+++ src/app/db/drizzle.ts
@@ -1,6 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { postsTable } from "./schema";
+import { postsTable, usersTable } from "./schema";
 import { config } from "dotenv";
 
 config({ path: ".env.local" });
@@ -8,5 +8,5 @@ config({ path: ".env.local" });
 const sql = neon(process.env.NEON_DATABASE_URL!);
 
 export const db = drizzle(sql, {
-  schema: { postsTable },
+  schema: { postsTable, usersTable },
 });
