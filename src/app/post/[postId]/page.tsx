7096c1b6eb03f0e0ae3d5d6d3b0524bd0b48c900
jefersonapps <jefersonplaynunes@gmail.com>
2024-10-19 14:09:10 -0300

    feat: add dynamic metadata on single post page

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,5 +1,34 @@
 import { PostDetails } from "./components/post-details";
 import { BackButton } from "./components/back-button";
+import { ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  { params }: { params: { postId: string } },
+  parent: ResolvingMetadata
+) {
+  const { postId } = params;
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  const post = await fetch(
+    `https://modela3dblog.vercel.app/api/posts/get-post?postId=${postId}&userId=jefersonapps`
+  ).then((res) => res.json());
+
+  return {
+    title: `${post ? post[0].author : "Usuário"} no Modela3D | Blog`,
+    description: post
+      ? post[0].content
+      : "Acesse para visualizar o conteúdo da postagem",
+    openGraph: {
+      images: [
+        post
+          ? post[0].userImageUrl
+          : "https://modela3dblog.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffavicon.28c6f610.ico&w=48&q=75",
+        ...previousImages,
+      ],
+    },
+  };
+}
 
 export default function Page({ params }: { params: { postId: string } }) {
   return (
