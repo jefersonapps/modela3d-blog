dd86aebc3ec6d26c4344de770fcbc1e9f87e1972
jefersonapps <jefersonplaynunes@gmail.com>
2024-10-18 22:15:06 -0300

    fix: try to fix users posts request

--- src/app/api/posts/get-user-posts/route.ts
+++ src/app/api/posts/get-user-posts/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const page = searchParams.get("page");
+  const page = parseInt(searchParams.get("page") || "1");
   const userId = searchParams.get("userId");
-  const pageSize = searchParams.get("pageSize");
+  const pageSize = parseInt(searchParams.get("pageSize") || "10");
   const searchQuery = searchParams.get("searchQuery");
 
   if (!page || !userId || !pageSize) {
@@ -15,9 +15,9 @@ export async function GET(request: Request) {
     );
   }
   const post = await getPostsOfUser({
-    page: parseInt(page),
+    page,
     userId,
-    pageSize: parseInt(pageSize),
+    pageSize,
     searchQuery,
   });
 
--- src/app/http/get-user-posts.ts
+++ src/app/http/get-user-posts.ts
@@ -11,6 +11,11 @@ export async function getUserPosts({
   pageSize?: number;
   searchQuery?: string | null;
 }): Promise<Post[] | null> {
+  console.log("userId", userId);
+  console.log("page", page);
+  console.log("pageSize", pageSize);
+  console.log("searchQuery", searchQuery);
+
   if (!userId || !page || !pageSize) {
     console.warn("No userId or page or pageSize provided.");
     return null;
@@ -20,11 +25,15 @@ export async function getUserPosts({
     `/api/posts/get-user-posts?userId=${userId}&page=${page}&pageSize=${pageSize}&searchQuery=${searchQuery}`
   );
 
+  console.log("response", response);
+
   if (!response.ok) {
     console.error("Failed to fetch posts");
     return null;
   }
 
   const data: Post[] = await response.json();
+
+  console.log("data", data);
   return data;
 }
